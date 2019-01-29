9ee3bf73964ebd97a4c720b8736b414f91d5f8fc
ashith <ashith.rahul@razorthink.com>
2019-01-29 20:39:02 +0530

    card deon

---
+++ src/components/Card/Card.js
@@ -0,0 +1,8 @@
+import classNames from 'classnames';
+import React from 'react';
+import * as styles from './Card.css';
+
+const Card = ({child,classes}) => (
+  <div className={classNames(styles.container)}>{child}</div>
+);
+export default Card;
--- src/pages/DashBoard/DashBoard.route.js
+++ src/pages/DashBoard/DashBoard.route.js
@@ -1,13 +1,32 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Card from '../../components/Card/Card';
 import NavBar from './Componets/NavBar/NavBar';
 import SideBar from './Componets/SideBar/SideBar';
 import { ADD_USER, DASHBOARD_URL } from './DashBoard.constants';
 
-const Home = () => <div>Home</div>;
+const Home = () => (
+  <div>
+    <Card child={<div>adsasdasda</div>} />
+    <Card child={<div>adsasdasda</div>} />
+    <Card child={<div>adsasdasda</div>} />
+    <Card child={<div>adsasdasda</div>} />
+    <Card child={<div>adsasdasda</div>} />
+    <Card child={<div>adsasdasda</div>} />
+    <Card child={<div>adsasdasda</div>} />
+    <Card child={<div>adsasdasda</div>} />
+    <Card child={<div>adsasdasda</div>} />
+    <Card child={<div>adsasdasda</div>} />
+    <Card child={<div>adsasdasda</div>} />
+    <Card child={<div>adsasdasda</div>} />
+    <Card child={<div>adsasdasda</div>} />
+    <Card child={<div>adsasdasda</div>} />
+  </div>
+  );
 const About = () => <div>About</div>;
 
-const divStyle = { display: 'flex', flexGrow: 1 };
+const divStyle = { display: 'flex', height: '100%' };
+const contentWrapper = { padding: '20px', height: 'calc(100% - 70px)', overflow: 'auto', width:'100%' };
 
 const DashBoard = () => (
   <Router>
@@ -15,8 +34,10 @@ const DashBoard = () => (
       <NavBar />
       <div style={divStyle}>
         <SideBar />
+        <div style={contentWrapper}>
         <Route path={ADD_USER} component={About} />
         <Route exact path={DASHBOARD_URL} component={Home} />
+        </div>
       </div>
     </Fragment>
   </Router>
